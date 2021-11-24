6a40b4239124b7ee665449448690f72f3dfc5489
sgdxbc <sgdxbc@gmail.com>
2021-11-24 09:41:59 +0000

    Update run script

---
+++ run/common.py
@@ -0,0 +1,99 @@
+import re
+import pathlib
+import shutil
+import sys
+import pyrem.host
+import pyrem.task
+
+
+proj_dir = '/home/cowsay/dsnet/'
+local_dir = '/work/dsnet/'
+log_dir = pathlib.Path() / 'logs'
+
+node = [
+    pyrem.host.LocalHost(),
+    pyrem.host.RemoteHost('nsl-node1'),
+    pyrem.host.RemoteHost('nsl-node2'),
+    pyrem.host.RemoteHost('nsl-node3'),
+    pyrem.host.RemoteHost('nsl-node4'),
+    pyrem.host.RemoteHost('nsl-node5'),
+]
+
+
+def setup(title):
+    print(title)
+    for line in open(pathlib.Path() / 'Makefile'):
+        if '-O3' in line and line.strip().startswith('#'):
+            print('Makefile is not in benchmark mode')
+            sys.exit(1)
+
+
+    shutil.rmtree(log_dir, ignore_errors=True)
+    log_dir.mkdir()
+    (log_dir / '.gitignore').write_text('*')
+
+
+    node[0].run([
+        'rsync', '-a', 
+        '--exclude', '.obj', 
+        '--exclude', 'bench/client',
+        '--exclude', 'bench/replica',
+        local_dir, f'nsl-node1:' + proj_dir[:-1]
+    ]).start(wait=True)
+    # TODO abort on compile fail
+    node[1].run(['make', '-j', '64', '-C', '/home/cowsay/dsnet', 'bench/client', 'bench/replica']).start(wait=True)
+
+    rsync_tasks = []
+    for node_index in (2, 3, 4, 5):
+        rsync_tasks += [
+            node[1].run([
+                'rsync', 
+                proj_dir + 'bench/client',
+                f'nsl-node{node_index}.d1:{proj_dir}/bench/client', 
+            ]),
+            node[1].run([
+                'rsync', 
+                proj_dir + 'bench/replica',
+                f'nsl-node{node_index}.d1:{proj_dir}/bench/replica', 
+            ]),
+            node[1].run([
+                'rsync', 
+                proj_dir + 'run/nsl.txt',
+                f'nsl-node{node_index}.d1:{proj_dir}/run/nsl.txt', 
+            ]),
+        ]
+    for task in rsync_tasks:
+        task.start()
+    for task in rsync_tasks:
+        task.wait()
+
+    print('Start')
+
+
+def wait_replica(replica_task, i):
+    replica_task[i].wait()
+    output = replica_task[i].return_values['stderr'].decode()
+    if re.search(f'Terminating on SIGTERM/SIGINT$', output, re.MULTILINE) is None:
+        print(f'Replica {i} not finish normally')
+        print(output)
+        sys.exit(1)
+    with open(pathlib.Path() / 'logs' / f'replica-{i}.txt', 'w') as log_file:
+        log_file.write(output)
+
+
+def wait_client(client_task):
+    throughput_sum = 0
+    median_latency_max = 0
+    for i, task in enumerate(client_task):
+        output = task.return_values['stderr'].decode()
+        match = re.search(r'Total throughput is (\d+) ops/sec$', output, re.MULTILINE)
+        if match is not None:
+            throughput_sum += int(match[1])
+        else:
+            print(f'warning: no data from client-{i}')
+            with open(pathlib.Path() / 'logs' / f'client-{i}.txt', 'w') as log_file:
+                log_file.write(output)
+            continue
+        match = re.search(r'Median latency is (\d+) us$', output, re.MULTILINE)
+        median_latency_max = max(median_latency_max, int(match[1]))
+    print(throughput_sum, median_latency_max)
--- run/tombft.py
+++ run/tombft.py
@@ -1,67 +1,12 @@
-import re
-import pathlib
-import shutil
 import sys
-import pyrem.host
+import pathlib
+sys.path.append(pathlib.Path() / 'run')
+import common
 import pyrem.task
 
-print('TOMBFT performance')
-for line in open(pathlib.Path() / 'Makefile'):
-    if '-O3' in line and line.strip().startswith('#'):
-        print('Makefile is not in benchmark mode')
-        sys.exit(1)
+common.setup('TOMBFT performance')
 
-proj_dir = '/home/cowsay/dsnet/'
-local_dir = '/work/dsnet/'
-log_dir = pathlib.Path() / 'logs'
-duration = 10
-
-shutil.rmtree(log_dir, ignore_errors=True)
-log_dir.mkdir()
-(log_dir / '.gitignore').write_text('*')
-
-node = [
-    pyrem.host.LocalHost(),
-    pyrem.host.RemoteHost('nsl-node1'),
-    pyrem.host.RemoteHost('nsl-node2'),
-    pyrem.host.RemoteHost('nsl-node3'),
-    pyrem.host.RemoteHost('nsl-node4'),
-    pyrem.host.RemoteHost('nsl-node5'),
-]
-
-node[0].run([
-    'rsync', '-a', 
-    '--exclude', '.obj', 
-    '--exclude', 'bench/client',
-    '--exclude', 'bench/replica',
-    local_dir, f'nsl-node1:' + proj_dir[:-1]
-]).start(wait=True)
-rval = node[1].run(['make', '-j', '64', '-C', '/home/cowsay/dsnet', 'bench/client', 'bench/replica']).start(wait=True)
-
-rsync_tasks = []
-for node_index in (2, 3, 4, 5):
-    rsync_tasks += [
-        node[1].run([
-            'rsync', 
-            proj_dir + 'bench/client',
-            f'nsl-node{node_index}.d1:{proj_dir}/bench/client', 
-        ]),
-        node[1].run([
-            'rsync', 
-            proj_dir + 'bench/replica',
-            f'nsl-node{node_index}.d1:{proj_dir}/bench/replica', 
-        ]),
-        node[1].run([
-            'rsync', 
-            proj_dir + 'run/nsl.txt',
-            f'nsl-node{node_index}.d1:{proj_dir}/run/nsl.txt', 
-        ]),
-    ]
-for task in rsync_tasks:
-    task.start()
-for task in rsync_tasks:
-    task.wait()
-
+duration = 20
 def replica_cmd(index):
     return [
         'timeout', f'{duration + 3}',
@@ -69,7 +14,7 @@ def replica_cmd(index):
         '-c', proj_dir + 'run/nsl.txt',
         '-m', 'tombft',
         '-i', f'{index}',
-        '-w', '27',
+        '-w', '24',
     ]
 client_cmd = [
     'timeout', f'{duration + 3}',
@@ -90,32 +35,8 @@ client_task = [
     node[5].run(client_cmd, return_output=True)
     for _ in range(20)
 ]
-print('Start')
 pyrem.task.Parallel(client_task).start(wait=True)
 
 for i in range(4):
-    # if i == 1: continue
-    replica_task[i].wait()
-    output = replica_task[i].return_values['stderr'].decode()
-    if re.search(f'Terminating on SIGTERM/SIGINT$', output, re.MULTILINE) is None:
-        print(f'Replica {i} not finish normally')
-        print(output)
-        sys.exit(1)
-    with open(pathlib.Path() / 'logs' / f'replica-{i}.txt', 'w') as log_file:
-        log_file.write(output)
-
-throughput_sum = 0
-median_latency_max = 0
-for i, task in enumerate(client_task):
-    output = task.return_values['stderr'].decode()
-    match = re.search(r'Total throughput is (\d+) ops/sec$', output, re.MULTILINE)
-    if match is not None:
-        throughput_sum += int(match[1])
-    else:
-        print(f'warning: no data from client-{i}')
-        with open(pathlib.Path() / 'logs' / f'client-{i}.txt', 'w') as log_file:
-            log_file.write(output)
-        continue
-    match = re.search(r'Median latency is (\d+) us$', output, re.MULTILINE)
-    median_latency_max = max(median_latency_max, int(match[1]))
-print(throughput_sum, median_latency_max)
+    common.wait_replica(i)
+common.wait_client()
